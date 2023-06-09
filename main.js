9ed4fd016c611cc8735128ddd7cfa914a272ca2c
Pandeige2803 <dedegetto_47@yahoo.com>
2023-06-09 09:44:45 +0800

    tes java script

---
+++ main.js
@@ -0,0 +1,28 @@
+// let nama = "dede sudiahna";
+// let usia = 30;
+// let tinggiBadan = 166.5;
+// let beratBadan ;
+// let pacar = 1;
+
+// beratBadan = 73
+
+// if(pacar == null) {
+//     pacar = "belum punya"
+// } else {
+//     pacar = "udah punya"
+// }
+// let saldoAwal = 50000
+// let saldoTambahan = 80000
+// const hutang = 30000
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang
+
+// const x = 5123
+// const y = 123128
+// const z = x / y 
+
+// alert(`hasil dari x = ${x} : y = ${y} adalah ${z}`)
+
+
+// alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`);
+// alert(`saldo awal saya sebesar Rp. ${saldoAwal} & saldo tambahan yang akan saya miliki sebesar Rp. ${saldoTambahan} jadi total saldo yang saya miliki adalah sebanyak Rp. ${saldoAkhir}`);
+
